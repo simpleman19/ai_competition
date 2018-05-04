f5c26d0fd006d7fa6409222caacfc547e906dab1
simpleman19 <chancert413@gmail.com>
2018-05-04 18:29:00 -0500

    Working on script

--- train.sh
+++ train.sh
@@ -5,6 +5,8 @@ git push origin --tags
 ssh 198.204.229.156 bash -c "'
 cd /home/simpleman19/ai_competition
 pwd
+. .env/bin/activate
+pip install -r requirements.txt
 git checkout master
 git pull
 git checkout ${uuid}
