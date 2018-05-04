53d49c5f723b3d1d8fce97505596135a3697eac7
simpleman19 <chancert413@gmail.com>
2018-05-04 18:26:41 -0500

    Working on script

--- train.sh
+++ train.sh
@@ -2,4 +2,9 @@
 uuid=$(uuidgen)
 git tag ${uuid}
 git push origin --tags
-ssh 198.204.229.156 bash -c "cd ai_competition && git pull && git checkout ${uuid} && python3.6 train.py ${uuid}"
+ssh 198.204.229.156 bash -c "'
+cd /home/simpleman19/ai_competition
+pwd
+git pull
+git checkout ${uuid}
+python3.6 train.py ${uuid}'"
