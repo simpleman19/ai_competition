d3763e5a51b272872b543c4da3323956a438947f
simpleman19 <chancert413@gmail.com>
2018-05-05 08:19:16 -0500

    Auto commit - Add model to train list

--- train.py
+++ train.py
@@ -96,4 +96,4 @@ if __name__ == '__main__':
         uuid = sys.argv[1]
     else:
         uuid = 'model'
-    train_model(files, 128, 4, .8, uuid=uuid)
+    train_model(files, 256, 4, .8, uuid=uuid)
