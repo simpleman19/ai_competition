44349c6848cc3cc607bbd02dba20aeee448b3968
simpleman19 <chancert413@gmail.com>
2018-05-05 17:31:44 -0500

    Auto commit - Add model to train list

--- model2.py
+++ model2.py
@@ -16,7 +16,7 @@ def compile_model():
     model.add(Dense(256, activation='relu'))
     model.add(Dense(128, activation='relu'))
     model.add(Dense(24, activation='softmax'))
-    sgd = SGD(lr=0.01, momentum=0.1, decay=0.0, nesterov=False)
+    sgd = SGD(lr=0.02, momentum=0.1, decay=0.0, nesterov=False)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return model
 
