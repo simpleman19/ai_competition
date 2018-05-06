a160d77681406174d0391a3998a7751722c09838
simpleman19 <chancert413@gmail.com>
2018-05-06 14:20:04 -0500

    Auto commit - Add model to train list

--- google_run.sh
+++ google_run.sh
@@ -4,23 +4,23 @@ name=training
 
 ip=$(gcloud compute instances describe ${name} --zone=${zone} | grep natIP: | sed 's/:/\n/g' | sed "s/ //g" | sed -n 2p)
 
+if [ -z "$ip" ]; then
+    echo "Could not find instance, creating a new one"
+    ./start_instance.sh ${name} ${zone}
+    ip=$(gcloud compute instances describe ${name} --zone=${zone} | grep natIP: | sed 's/:/\n/g' | sed "s/ //g" | sed -n 2p)
+    echo "Instance ip: ${ip}"
+    echo "Setting up instance: "
+    ./setup_google.sh ${ip}
+fi
+
+./train_google.sh $ip
 
 if [ "$1" == "-d" ]; then
     if [ -z "$ip" ]; then
         echo "Didn't find instance to delete"
-
     else
         echo "Deleting Instance: "
-        gcloud compute instances delete ${name} --zone=${zone}
+        gcloud -q compute instances delete ${name} --zone=${zone}
+        ssh-keygen -R ${ip}
     fi
-else
-    if [ -z "$ip" ]; then
-        echo "Could not find instance, creating a new one"
-        ./start_instance.sh ${name} ${zone}
-        ip=$(gcloud compute instances describe ${name} --zone=${zone} | grep natIP: | sed 's/:/\n/g' | sed "s/ //g" | sed -n 2p)
-        echo "Instance ip: ${ip}"
-        echo "Setting up instance: "
-        ./setup_google.sh ${ip}
-    fi
-    ./train_google.sh $ip
 fi
