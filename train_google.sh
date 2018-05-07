13be5604b0277566fafb51f6ef49f3a71487798d
simpleman19 <chancert413@gmail.com>
2018-05-07 16:30:15 -0500

    Auto commit - Add model to train list

--- train_google.sh
+++ train_google.sh
@@ -4,7 +4,7 @@ file="train_list"
 lines=`cat ${file}`
 for uuid in ${lines}; do
         ssh -i ~/.ssh/google_compute_engine chancert413_gmail_com@${host} bash -c "'
-cd /home/chancert413_google_com/ai_competition
+cd /home/chancert413_gmail_com/ai_competition
 pwd
 . .env/bin/activate
 git checkout master
@@ -16,9 +16,9 @@ done
 
 ssh -i ~/.ssh/google_compute_engine chancert413_gmail_com@${host}:'/home/chancert413_gmail_com/ai_competition/*.{h5,png,log}' archive/
 ssh -i ~/.ssh/google_compute_engine chancert413_gmail_com@${host} bash -c "'
-rm /home/simpleman19/ai_competition/*.h5
-rm /home/simpleman19/ai_competition/*.png
-rm /home/simpleman19/ai_competition/*.log
+rm /home/chancert413_gmail_com/ai_competition/*.h5
+rm /home/chancert413_gmail_com/ai_competition/*.png
+rm /home/chancert413_gmail_com/ai_competition/*.log
 '"
 rm train_list
 touch train_list
