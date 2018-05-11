a83ef64ca32d25cf77a9f4e45d3d7c88d8bdcb92
simpleman19 <chancert413@gmail.com>
2018-05-11 18:20:38 -0500

    Auto commit - Add model to train list

--- model3.py
+++ model3.py
@@ -6,9 +6,9 @@ from keras import regularizers
 
 def compile_model():
     model = Sequential()
-    model.add(LSTM(300, input_shape=(1024, 2)))
+    model.add(LSTM(32, input_shape=(1024, 2)))
     model.add(Dense(24, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 
