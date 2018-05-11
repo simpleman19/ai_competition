e9c1b619678f2ca3ae7533eee9c312224d2f6b6b
simpleman19 <chancert413@gmail.com>
2018-05-10 20:21:45 -0500

    Auto commit - Add model to train list

---
+++ model3.py
@@ -0,0 +1,16 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from keras.optimizers import Adam, SGD
+from keras import regularizers
+
+
+def compile_model():
+    model = Sequential()
+    model.add(LSTM(1024))
+    model.add(Dense(24, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+if __name__ == '__main__':
+    compile_model()
--- train.py
+++ train.py
@@ -53,11 +53,56 @@ def train_model(filenames, train_names, batch_size, epochs, file_iterations, tra
             ev.append(model.evaluate(shuffled_data_flat, shuffled_one_hot)[1])
             shuffled_data_flat, shuffled_one_hot = None, None
     time = datetime.datetime.now()
-    scores = model.evaluate(test_data, test_labels)
+    shuffled_data_flat, shuffled_one_hot = load_data(train_names[0])
+    scores = model.evaluate(shuffled_data_flat, shuffled_one_hot)
     model.save('{date:%Y-%m-%d %H:%M:%S}-{uuid}-{score:1.4f}.h5'.format(uuid=uuid, date=time, score=scores[1] * 100))
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
     plot(loss, acc, ev, time, uuid)
 
+
+def train_lstm(filenames, train_names, batch_size, epochs, file_iterations, train_count=None, uuid=None):
+    model = compile_model()
+    test_data = None
+    test_labels = None
+    loss = []
+    acc = []
+    ev = []
+    for f in range(0, file_iterations):
+        print('-- File Iteration -- {}'.format(f + 1))
+        for file in filenames:
+            print('-- New File -- {}'.format(file))
+            shuffled_data_flat, shuffled_one_hot = load_data(file)
+            if train_count is None:
+                train_count = len(shuffled_one_hot)
+            batches = int(math.floor(train_count / batch_size))
+            for e in range(0, epochs):
+                print('Epoch {}/{}'.format(e+1, epochs))
+                for i in range(0, batches - 1):
+                    metrics = model.train_on_batch(shuffled_data_flat[i*batch_size:(i+1)*batch_size, :],
+                                         shuffled_one_hot[i*batch_size:(i+1)*batch_size])
+                    print('{:6d} / {:6d} - {:5s} {:1.4f} - {:5s} {:1.4f}'.format((i+1)*batch_size,
+                                                                                 train_count,
+                                                                                 model.metrics_names[0], metrics[0],
+                                                                                 model.metrics_names[1], metrics[1]))
+                    loss.append(metrics[0])
+                    acc.append(metrics[1])
+                metrics = model.train_on_batch(shuffled_data_flat[(batches-1) * batch_size:train_count, :],
+                                            shuffled_one_hot[(batches-1) * batch_size:train_count])
+                print('{:6d} / {:6d} - {:5s} {:1.4f} - {:5s} {:1.4f}'.format(train_count,
+                                                                             train_count,
+                                                                             model.metrics_names[0], metrics[0],
+                                                                             model.metrics_names[1], metrics[1]))
+                loss.append(metrics[0])
+                acc.append(metrics[1])
+            shuffled_data_flat, shuffled_one_hot = None, None
+            shuffled_data_flat, shuffled_one_hot = load_data(train_names[0])
+            ev.append(model.evaluate(shuffled_data_flat, shuffled_one_hot)[1])
+            shuffled_data_flat, shuffled_one_hot = None, None
+    time = datetime.datetime.now()
+    scores = model.evaluate(test_data, test_labels)
+    model.save('{date:%Y-%m-%d %H:%M:%S}-{uuid}-{score:1.4f}.h5'.format(uuid=uuid, date=time, score=scores[1] * 100))
+    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    plot(loss, acc, ev, time, uuid)
 
 def plot(loss, acc, ev, time, uuid):
     plt.plot(loss)
@@ -106,4 +151,4 @@ if __name__ == '__main__':
         uuid = sys.argv[1]
     else:
         uuid = 'model'
-    train_model(files, train_names, 64, 2, 1, uuid=uuid)
+    train_model(files, train_names, 128, 2, 1, uuid=uuid)
