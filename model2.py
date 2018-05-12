a7f26c208c969a77face57a7a10db993b6441249
simpleman19 <chancert413@gmail.com>
2018-05-12 13:13:23 -0500

    Auto commit - Add model to train list

--- model2.py
+++ model2.py
@@ -8,13 +8,13 @@ def compile_model():
     model = Sequential()
     model.add(Dense(2048,
                     input_dim=2048,
-                    activation='relu'
+                    activation='tanh'
                     ))
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1024, activation='tanh'))
+    model.add(Dense(512, activation='tanh'))
     # model.add(Dropout(.1))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(256, activation='tanh'))
+    model.add(Dense(128, activation='tanh'))
     model.add(Dense(24, activation='softmax'))
     sgd = SGD(lr=0.02, momentum=0.2, decay=0.0, nesterov=False)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
