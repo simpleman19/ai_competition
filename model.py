e510a7c14074293c3705d93d7f81966842300dd0
simpleman19 <chance.turner@eagles.oc.edu>
2018-07-17 18:04:25 -0500

    Auto commit - Add model to train list

--- model.py
+++ model.py
@@ -25,10 +25,9 @@ def compile_model():
     model.add(PReLU())
     model.add(Dense(8192, activation='linear'))
     model.add(PReLU())
-    model.add(Dense(8192, activation='softplus'))
-    model.add(Dense(8192, activation='softplus'))
+    model.add(Dense(4096, activation='softplus'))
     model.add(Dense(24, activation='softmax'))
-    adam = Adam(lr=.0005, beta_1=.9, beta_2=.98, decay=0, amsgrad=True)
+    adam = Adam(lr=.0005, beta_1=.9, beta_2=.95, decay=0, amsgrad=True)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy', top_2])
     # scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1))
     return model, scaler
